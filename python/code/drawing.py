5309a6d131a3078d9e1171b7a814a7e95386d7cc
David F. Barrero <david.barrero@gmail.com>
2023-11-16 18:48:42 +0100

    Python updated

---
+++ python/code/drawing.py
@@ -0,0 +1,17 @@
+import arcade
+
+WIDTH = 600
+HEIGHT = 800
+
+arcade.open_window(WIDTH, HEIGHT, "Drawing Example")
+
+arcade.set_background_color(arcade.color.WHITE)
+
+arcade.start_render()
+
+# Drawing here
+
+arcade.finish_render()
+
+arcade.run()
+
