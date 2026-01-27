bed8e6e2d6fd0b385fb7fa89473af36e5f7676bb
David F. Barrero <david.barrero@gmail.com>
2026-01-27 17:59:46 +0100

    Imports good practices added

---
+++ python/code/imports.py
@@ -0,0 +1,19 @@
+"""
+Example module with well-organized imports
+"""
+
+# Standard library
+import json
+import os
+
+# Third-party libraries
+import arcade
+import numpy as np
+
+# Local modules
+from config import SETTINGS
+from utils.helpers import process_data
+
+# Rest of the code
+def main():
+    pass
