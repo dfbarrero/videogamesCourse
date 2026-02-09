514dd7bc33c7485ba12fe2b3a42c83d6ad079b66
David F. Barrero <david.barrero@gmail.com>
2026-02-09 17:45:58 +0100

    Minor fix

--- morePython/code/music.py
+++ morePython/code/music.py
@@ -5,8 +5,12 @@ HEIGHT = 600
 
 arcade.open_window(WIDTH, HEIGHT, "Example of sound in Arcade")
 
-music = arcade.load_sound(":resources:/music/1918.mp3", streaming=True)
+music = arcade.load_sound(
+        ":resources:/music/1918.mp3", 
+        streaming=True)
+# music = Sound(":resources:/music/1918.mp3", streaming=True) 
 arcade.play_sound(music)
+# music.play() # Object-oriented style
 
 arcade.start_render()
 arcade.draw_text("Enjoy!", 350, 300, arcade.color.WHITE)
